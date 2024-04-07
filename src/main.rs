1de63b4681b101afd4c148d19608d48785b37ef1
Henry Steinberg <26606943+HenryPTS@users.noreply.github.com>
2024-04-07 09:13:36 +0100

    feat: set up routing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use rocket::{get, launch, routes};
+use rocket_dyn_templates::{context, Template};
+use serde::{Deserialize, Serialize};
+use std::fs;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Chapter {
+    it: Vec<Vec<String>>,
+    en: Vec<Vec<String>>,
+}
+
+#[get("/")]
+fn index() -> Template {
+    let chapter_str = fs::read_to_string("./json/chapters/1.json").expect("unable to read file");
+    let chapter: Chapter = serde_json::from_str(&chapter_str).expect("json was not well formatted");
+    Template::render(
+        "index",
+        context! { chapter: chapter, chapter_str: chapter_str },
+    )
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![index])
+        .attach(Template::fairing())
 }
