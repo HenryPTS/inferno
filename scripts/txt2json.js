5c158b0c4c8307231de8003e86cc2b8689bd7c30
Henry Steinberg <henry@steinberg.sh>
2024-04-07 06:32:37 +0100

    feat: first commit

---
+++ scripts/txt2json.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+
+function readCanto(lang = "en", num = 1) {
+  const canto = fs
+    .readFileSync(`txt/chapters/${lang}/${num}.txt`)
+    .toString("utf-8")
+    .split("\n");
+  const cantoArr = [[]];
+  for (const line of canto) {
+    if (line === "") {
+      cantoArr.push([]);
+    } else {
+      const index = cantoArr.length - 1;
+      cantoArr[index].push(line.trim());
+    }
+  }
+  return cantoArr;
+}
+
+const json = [];
+
+for (let i = 1; i < 35; i++) {
+  const it = readCanto("it", i);
+  const en = readCanto("en", i);
+  fs.writeFileSync(
+    `json/chapters/${i}.json`,
+    JSON.stringify({ it, en }, undefined, 0)
+  );
+  json.push({ it, en });
+}
